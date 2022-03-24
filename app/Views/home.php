b15844e3087d0ea604f5e878211871a2f9dd76a6
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-03-24 08:47:15 +0700

    tambah home

---
+++ app/Views/home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+    <title>Rizal Abdul Basith</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Rijal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li> -->
+                </ul>
+                <!-- <span class="navbar-text">
+                    Navbar text with an inline element
+                </span> -->
+            </div>
+        </div>
+    </nav>
+    <div class="container my-4">
+        <div class="p-5 mb-4 bg-light border rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Rizal Abdul Basith</h1>
+                <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quisquam libero reiciendis consequuntur dolor! Deleniti nam sunt fugiat qui error, exercitationem vel optio, earum pariatur voluptatem, a assumenda obcaecati odit.</p>
+                <button class="btn btn-primary btn-lg" type="button">Coba</button>
+            </div>
+        </div>
+        <div class="card my-3">
+            <div class="card-body">
+                <h3 class="card-title">acascadasd</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore voluptate ad iure animi perferendis, esse repellendus sequi praesentium, incidunt deserunt itaque in voluptatum dolorem possimus numquam? Laboriosam, facilis atque?</p>
+            </div>
+        </div>
+        <div class="card my-3">
+            <div class="card-body">
+                <h3 class="card-title">acascadasd</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore voluptate ad iure animi perferendis, esse repellendus sequi praesentium, incidunt deserunt itaque in voluptatum dolorem possimus numquam? Laboriosam, facilis atque?</p>
+            </div>
+        </div>
+        <div class="card my-3">
+            <div class="card-body">
+                <h3 class="card-title">acascadasd</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore voluptate ad iure animi perferendis, esse repellendus sequi praesentium, incidunt deserunt itaque in voluptatum dolorem possimus numquam? Laboriosam, facilis atque?</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="<?= base_url('js/bootstrap.bundle.min.js') ?>" />
+</body>
+
+</html>
